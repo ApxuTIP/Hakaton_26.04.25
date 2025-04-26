723b6fbe52878059bc23edea812ea7bd4515c95b
Цапик Трофим <t.tsapik@domain.local>
2025-04-26 16:36:48 +0300

    Stage-One

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Header from "./Header.jsx"
+import Sidebar from "./Sidebar.jsx";
+
+function App() {
+  return(
+    <>
+        <Header/>
+        <Sidebar/>
+    </>
+
+  );
+}
+
+export default App
