8036035aab365383b89ea660bf33213a6c2f1256
Цапик Трофим <t.tsapik@domain.local>
2025-04-26 19:24:12 +0300

    fix-sedebar-and-buttom

--- src/Button.jsx
+++ src/Button.jsx
@@ -7,10 +7,10 @@ function Button() {
     return (
         <div className="main_button">
             <div>
-            <NativeSelect label="Уровень" description="" data={['Low', 'Middle', 'High']} />
+            <NativeSelect label="Уровень" description="" data={['Low', 'Medium', 'High']} />
             </div>
             <div>
-            <NativeSelect label="Язык/Навык" description="" data={['Low', 'Middle', 'High']} />
+            <NativeSelect label="Язык/Навык" description="" data={["Analytical thinking","Algorithms","Backend","Business Modeling","C","C++","Change Management","Code review","Cryptography","DB & Data","DevOps","Functional programming","Graphics","Information Security","Java","Leadership","Linux","ML & AI","Math","Network & system administration","OOP","Parallel computing","Python","Requirements Analysis","SQL","Shell/Bash","Software architecture","Structured programming","Systems Integration","Team work","Types and data structures","Web"]} />
             </div>
         </div>
     );
